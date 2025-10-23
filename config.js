1d5b40da8f2c5fdf68f38a134bae2cb5f0dc7fe9
Sanmin-Jang <240532@astanait.edu.kz>
2025-10-23 15:44:37 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
   // Замените на ваш URL веб-приложения Google Apps Script
-  API_URL: 'https://script.google.com/macros/s/AKfycbwNUPGee8tJkEoDBbtWzdUxs8-aWw2hyEtmDas8iK7OFIMLn1rhqrmXr_A1JQOboaua/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyiA3mCF9vPJKNeM6XRCRJNgqFAJ5JKk4vPIR13wzqcwtQw2e0GJAY4zizMXvdvvfw/exec',
 
   // Список операторов
   OPERATORS: [
